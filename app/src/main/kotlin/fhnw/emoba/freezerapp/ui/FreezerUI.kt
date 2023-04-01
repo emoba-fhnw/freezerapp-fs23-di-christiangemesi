1665130fe38ef046b2397bb8771f534fc54ba71d
Christians Desktop <christian.gemesi@students.fhnw.ch>
2023-04-01 16:57:22 +0200

    added header

--- app/src/main/kotlin/fhnw/emoba/freezerapp/ui/FreezerUI.kt
+++ app/src/main/kotlin/fhnw/emoba/freezerapp/ui/FreezerUI.kt
@@ -1,26 +1,38 @@
 package fhnw.emoba.freezerapp.ui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import fhnw.emoba.freezerapp.model.FreezerModel
 
 
 @Composable
-fun AppUI(model : FreezerModel){
-    with(model){
-        Box(contentAlignment = Alignment.Center,
-            modifier         = Modifier.fillMaxSize()
-        ){
-            Text(text  = title,
-                 style = TextStyle(fontSize = 42.sp))
+fun AppUI(model: FreezerModel) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        IconButton(
+            onClick = { println("Menu clicked!") },
+            modifier = Modifier.padding(start = 8.dp, end = 8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Menu,
+                contentDescription = "Open navigation menu"
+            )
         }
+        Text(
+            text = "FreezerApp",
+            modifier = Modifier.weight(1f),
+            style = TextStyle(fontSize = 20.sp),
+            textAlign = TextAlign.Left
+        )
     }
 }
 

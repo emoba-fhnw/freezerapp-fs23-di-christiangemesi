8da38419d329c2d12adaf8cae5376309e4457b7a
Christians Desktop <christian.gemesi@students.fhnw.ch>
2023-04-09 09:23:15 +0200

    added search field

--- app/src/main/kotlin/fhnw/emoba/freezerapp/ui/Tabs/SongsTab.kt
+++ app/src/main/kotlin/fhnw/emoba/freezerapp/ui/Tabs/SongsTab.kt
@@ -1,13 +1,24 @@
 package fhnw.emoba.freezerapp.ui.Tabs
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import fhnw.emoba.freezerapp.model.FreezerModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SongsTab(model: FreezerModel) {
     Column {
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text("Search") },
+            modifier = Modifier.fillMaxWidth()
+        )
         Text("Songsssss")
     }
 }

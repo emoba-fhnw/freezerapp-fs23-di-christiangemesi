884938ff365f4404e0cf99911f7f37c7b2cc3a71
Christians Desktop <christian.gemesi@students.fhnw.ch>
2023-04-09 12:45:41 +0200

    working on displaying the radio tab

--- app/src/main/kotlin/fhnw/emoba/freezerapp/ui/Tabs/RadioTab.kt
+++ app/src/main/kotlin/fhnw/emoba/freezerapp/ui/Tabs/RadioTab.kt
@@ -1,8 +1,13 @@
 package fhnw.emoba.freezerapp.ui.Tabs
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import fhnw.emoba.freezerapp.data.Radio
 import fhnw.emoba.freezerapp.model.FreezerModel
 
 @Composable
@@ -10,7 +15,26 @@ fun RadioTab(model: FreezerModel) {
 
     Column {
         Text("Radioooooo")
-        val movie = model.movieList
-        Text(movie.toString())
+        val radioList = model.movieList
+        radioList.forEach { radio ->
+            Text(radio.title)
+        }
+    }
+
+
+}
+
+@Composable
+fun RadioItem(radio: Radio) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+
+        Column {
+            Text(text = radio.title)
+            // add more text components to display other song details
+        }
     }
 }

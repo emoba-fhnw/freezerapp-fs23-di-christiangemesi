a02ae719066e5dc15242e5eae90dbbfe7181df59
Christians Desktop <christian.gemesi@students.fhnw.ch>
2023-04-10 09:22:20 +0200

    refactoring of songs tab

--- app/src/main/kotlin/fhnw/emoba/freezerapp/model/FreezerModel.kt
+++ app/src/main/kotlin/fhnw/emoba/freezerapp/model/FreezerModel.kt
@@ -112,6 +112,7 @@ class FreezerModel(private val service: MovieService) {
     fun loadSongAsync(songQuery: String) {
         modelScope.launch {
             songsList = service.filterSongBySearch(songQuery)
+            println("Songslist from backend: " + songsList.size) //25 inside
         }
     }
 
--- app/src/main/kotlin/fhnw/emoba/freezerapp/ui/Tabs/SongsTab.kt
+++ app/src/main/kotlin/fhnw/emoba/freezerapp/ui/Tabs/SongsTab.kt
@@ -1,5 +1,6 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -18,7 +19,6 @@ import androidx.compose.ui.Alignment
 fun SongsTab(model: FreezerModel) {
     var searchText by remember { mutableStateOf("") }
     var songs by remember { mutableStateOf(emptyList<Song>()) }
-    var currentIndex by remember { mutableStateOf(0) }
 
     Row(
         modifier = Modifier.fillMaxWidth(),
@@ -40,69 +40,73 @@ fun SongsTab(model: FreezerModel) {
         }
     }
 
+    LaunchedEffect(searchText) {
+        val newSongs = model.songsList
+        songs = newSongs
+    }
+
     // Display the search results
-    Column {
-        songs = model.songsList // update the list of songs when it changes
-        songs.forEachIndexed { index, song ->
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                song.imageBitmap?.let { bitmap ->
-                    Image(
-                        bitmap,
-                        contentDescription = "Album Image",
-                        Modifier
-                            .size(60.dp)
-                            .clip(RoundedCornerShape(8.dp)),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-                Spacer(Modifier.width(16.dp))
-                Column(Modifier.weight(1f)) {
-                    Text(song.title)
-                    Spacer(Modifier.height(4.dp))
-                    Text(song.artist)
-                }
-                IconButton(onClick = {
-                    model.fromStart()
-                }) {
-                    Icon(
-                        Icons.Filled.Replay,
-                        contentDescription = "Restart Song"
-                    )
-                }
-                IconButton(onClick = {
-                    if (model.playerIsReady && model.currentSong == song) { // if playing and current song is selected, pause
-                        model.pausePlayer()
-                    } else { // if not playing or different song is selected, start playing
-                        model.startPlayer(song)
-                    }
-                }) {
-                    Icon(
-                        if (model.playerIsReady && model.currentSong == song) Icons.Filled.Pause else Icons.Filled.PlayArrow, // change icon based on playing status and current song
-                        contentDescription = if (model.playerIsReady && model.currentSong == song) "Pause Song" else "Play Song"
-                    )
-                }
-                IconButton(onClick = {
-                    currentIndex = (index + 1) % songs.size
-                    model.startPlayer(songs[currentIndex])
-                }) {
-                    Icon(
-                        Icons.Filled.SkipNext,
-                        contentDescription = "Next Song"
-                    )
-                }
-                IconButton(onClick = {
-                    song.isFavorite = !song.isFavorite
-                    model.toggleFavorite(song)
-                }) {
-                    Icon(
-                        if (song.isFavorite) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
-                        contentDescription = if (song.isFavorite) "Remove from favorites" else "Add to favorites"
-                    )
-                }
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(vertical = 8.dp)
+    ) {
+        songs.forEach { song ->
+            item {
+                SongItem(song, model) // pass the model instance
             }
+        }
+    }
+}
+
+
+
+@Composable
+fun SongItem(song: Song, model: FreezerModel) {
+    var isFavorite by remember { mutableStateOf(song.isFavorite) }
+    var isPlaying by remember { mutableStateOf(model.currentSong == song && !model.playerIsReady) } // track playing status
+
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        song.imageBitmap?.let { bitmap ->
+            Image(
+                bitmap,
+                contentDescription = "Album Image",
+                Modifier
+                    .size(60.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+        }
+        Spacer(Modifier.width(16.dp))
+        Column(Modifier.weight(1f)) {
+            Text(song.title)
+            Spacer(Modifier.height(4.dp))
+            Text(song.artist)
+        }
+        IconButton(onClick = {
+            isPlaying = !isPlaying
+            if (isPlaying) {
+                model.startPlayer(song)
+            } else {
+                model.pausePlayer()
+            }
+        }) {
+            Icon(
+                if (isPlaying) Icons.Filled.Pause else Icons.Filled.PlayArrow,
+                contentDescription = if (isPlaying) "Pause Song" else "Play Song"
+            )
+        }
+        IconButton(onClick = {
+            isFavorite = !isFavorite
+            model.toggleFavorite(song)
+        }) {
+            Icon(
+                if (isFavorite) Icons.Filled.Favorite else Icons.Filled.FavoriteBorder,
+                contentDescription = if (isFavorite) "Remove from favorites" else "Add to favorites"
+            )
         }
     }
 }
